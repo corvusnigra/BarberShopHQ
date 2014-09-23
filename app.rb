2052a629d6a9ca8ceb789b95f56af6ef2c6a1c8e
Uriy <v-78@yandex.ru>
2014-09-24 01:01:36 +0400

    update index view

--- app.rb
+++ app.rb
@@ -11,5 +11,6 @@ end
 class Barber < ActiveRecord::Base
 end
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	@barbers=Barber.all
+	erb :index			
 end
--- db/migrate/20140922211806_create_clients.rb
+++ db/migrate/20140922211806_create_clients.rb
@@ -1,6 +1,6 @@
 class CreateClients < ActiveRecord::Migration
   def change
-  	create_table :clients do |t|
+  	create_table :client do |t|
      
     t.text :name
     t.text :phone
